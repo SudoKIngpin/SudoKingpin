0382a75d5e432cad1c942d214da0b9a256c5ed1d
sudoKIngpin <wd51886@gmail.com>
2022-04-17 13:48:14 +0530

    All errors fixed & rickroll added

--- app.py
+++ app.py
@@ -5,3 +5,20 @@ app=Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/ContactMe')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/robots')
+
+def robot():
+    return render_template('/static/robots.txt')
+
+@app.route('/magic')
+
+def magic():
+    return render_template('button.htm')
+# app.run(debug=True)
+
+
